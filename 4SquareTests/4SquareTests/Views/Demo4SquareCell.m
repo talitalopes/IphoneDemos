020a96c2be7e140fe282dd1a3fc570cd512ae7b6
Talita Lopes <talitalopes@gmail.com>
2013-10-17 22:06:32 -0400

    Adicionada célula customizada para a tabela

---
+++ 4SquareTests/4SquareTests/Views/Demo4SquareCell.m
@@ -0,0 +1,29 @@
+//
+//  Demo4SquareCell.m
+//  4SquareTests
+//
+//  Created by Talita Gomes on 10/17/13.
+//  Copyright (c) 2013 Talita Gomes. All rights reserved.
+//
+
+#import "Demo4SquareCell.h"
+
+@implementation Demo4SquareCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
