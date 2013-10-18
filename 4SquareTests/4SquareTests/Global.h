ef3de389f27dddc7b0cae8541bbda9bc37e7aa17
Talita Lopes <talitalopes@gmail.com>
2013-10-17 22:31:15 -0400

    Adicionada url, client_id e client_secret para foursquare

---
+++ 4SquareTests/4SquareTests/Global.h
@@ -0,0 +1,13 @@
+//
+//  Global.h
+//  4SquareTests
+//
+//  Created by Talita Gomes on 10/17/13.
+//  Copyright (c) 2013 Talita Gomes. All rights reserved.
+//
+
+// example: https://api.foursquare.com/v2/specials/search?ll=40.7,-74&client_id=R0JRITCXSDCLZH0OJ4MHBKUI4UJO0EDWL2XOGHY2152TOP3T&client_secret=PGNAAQVN3ET5EYZBBLKP5SRUNLTA0W3ICSOUYAGVYS3F2RRC&v=20131017
+#define K_FOURSQUARE_SEARCH_SPECIALS @"https://api.foursquare.com/v2/specials/search?ll=40.7,-74&client_id=%@&client_secret=%@&v=%@"
+
+#define K_FOURSQUARE_CLIENT_ID @"R0JRITCXSDCLZH0OJ4MHBKUI4UJO0EDWL2XOGHY2152TOP3T"
+#define K_FOURSQUARE_CLIENT_SECRET @"PGNAAQVN3ET5EYZBBLKP5SRUNLTA0W3ICSOUYAGVYS3F2RRC"
