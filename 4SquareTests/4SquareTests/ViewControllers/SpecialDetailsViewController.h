2d5b754d933f19444a09997de5035ad01789a3be
Talita Lopes <talitalopes@gmail.com>
2013-10-20 10:56:33 -0400

    Adicionada view controller para a tela de specials

---
+++ 4SquareTests/4SquareTests/ViewControllers/SpecialDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SpecialDetailsViewController.h
+//  4SquareTests
+//
+//  Created by Talita Gomes on 10/20/13.
+//  Copyright (c) 2013 Talita Gomes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SpecialDetailsViewController : UIViewController
+
+@end
